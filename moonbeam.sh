a70e875f7f67c7ff1e0624d9f176a7453ee7bffb
sankar.boro <sankar.boro@zeeve.io>
2023-02-07 18:31:28 +0530

    run moonbeam

--- moonbeam.sh
+++ moonbeam.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-function hello() 
-{
-    echo "Running moonbeam..."
-}
+CHAIN=${1}
+PWD=$(pwd)
+# cd "${CHAIN}-data"
+echo ${CHAIN} ${PWD}
+# wget https://github.com/PureStake/moonbeam/releases/download/v0.29.0/moonbeam
+# cd ..
+echo "Running moonbeam... ${CHAIN}"
+
+# ${cwd}/${chain}-data/${chain} \
+moonbeam \
+--port 30333 \
+--rpc-port 9933 \
+--ws-port 9944 \
+--execution wasm \
+--wasm-execution compiled \
+--state-pruning=archive \
+--trie-cache-size 0 \
+--db-cache 6000 \
+--base-path ${PWD}/${CHAIN}-data \
+--chain ${CHAIN} \
+--name "${CHAIN}-full-node" \
+--unsafe-rpc-external \
+--unsafe-ws-external \
+--rpc-cors all \
+-- \
+--port 30334 \
+--rpc-port 9934 \
+--ws-port 9945 \
+--execution wasm \
+--name "${chain}-full-node (Embedded Relay)"
--- parachain.sh
+++ parachain.sh
@@ -2,11 +2,15 @@
 
 ARGS=$#
 
-if [[ ${ARGS} -ne 2 ]]
-then
-    echo "Usage: ./parachain.sh -c moonbeam";
-    exit 1;
-fi
+function init() {
+    if [[ ${ARGS} -ne 2 ]]
+    then
+        echo "Usage: ./parachain.sh -c moonbeam";
+        exit 1;
+    fi
+}
+
+init
 
 while getopts c:n: option
 do 
@@ -17,19 +21,13 @@ do
     esac
 done
 
-CUR_DIR=$(pwd)
+# CUR_DIR=$(pwd)
 
-NEW_DIR="${chain}-data"
-rm -rf $NEW_DIR;
-mkdir $NEW_DIR;
+CHAIN_DIR="${chain}-data"
+rm -rf $CHAIN_DIR
+mkdir $CHAIN_DIR;
+chmod 777 $CHAIN_DIR
 
-source $(dirname "$0")/${chain}.sh
-hello
-# if [[ ${chain} -eq "moonbeam" ]]
-# then
-#     echo "Username is ${chain}";
-#     ${hello}
-#     exit 0;
-# fi
+source $(dirname "$0")/${chain}.sh ${chain}
 
 
