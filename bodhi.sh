7cbbd4c58083534eb6be0918e45c72f41aef80a1
sankar.boro <sankar.boro@zeeve.io>
2023-05-09 08:39:50 +0530

    one place

--- bodhi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "installing @microsoft/rush..."
-sudo npm install -g @microsoft/rush
-
-git clone GitHub - AcalaNetwork/bodhi.js 
-cd bodhi.js && git submodule update --init --recursive
-rush update && rush build -t @acala-network/eth-rpc-adapter
-npx @acala-network/eth-rpc-adapter -l -e ws://localhost:9944
