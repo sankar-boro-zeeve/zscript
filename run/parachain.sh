cca2cd5c55f54e42e72c93db7939c12b7c401042
Sankar boro <sankar.boro@zeeve.io>
2023-09-20 21:20:04 +0530

    download code instead checking local file

--- run/parachain.sh
+++ run/parachain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 run_zombienet() {
-  cd $HOME/zn
-  node ./dist/cli.js spawn --provider native $1
+  data=$(jq '.parachains[0].collators[0].command = "polkadot"' ./run/test.json | jq '.' > $HOME/para_test.json)
+  node $HOME/zn/dist/cli.js spawn --provider native $HOME/para_test.json
 }
--- run_parachain.sh
+++ run_parachain.sh
@@ -4,12 +4,12 @@ source installations/install.sh
 source installations/git_packages.sh
 source run/parachain.sh
 
-cd $HOME
-install_linux_packages
-install_rust
-install_nvm
-install_nodejs18
-install_polkadot
-install_zombienet
+# cd $HOME
+# install_linux_packages
+# install_rust
+# install_nvm
+# install_nodejs18
+# install_polkadot
+# install_zombienet
 
-run_zombienet $HOME/zeeve/zscript/examples/parachain.json
+run_zombienet $HOME/zeeve/zscript/examples/parachain-config.json
