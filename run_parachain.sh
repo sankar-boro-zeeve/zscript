f6fbca10464398fa16262a8d7610f591f0d6ae13
Sankar boro <sankar.boro@zeeve.io>
2023-09-22 11:38:30 +0530

    install

--- run_parachain.sh
+++ run_parachain.sh
@@ -4,13 +4,13 @@ source installations/install.sh
 source installations/git_packages.sh
 source run/parachain.sh
 
-# cd $HOME
-# install_linux_packages
-# install_rust
-# install_nvm
-# install_nodejs18
-# install_polkadot
-# install_zombienet
+cd $HOME
+install_linux_packages
+install_rust
+install_nvm
+install_nodejs18
+install_polkadot
+install_zombienet
 
 get_parachain_chain_spec
 run_zombienet
