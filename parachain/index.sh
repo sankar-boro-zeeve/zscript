b74bff5cf228a40230ab5b866d9be513b8233442
Sankar boro <sankar.boro@zeeve.io>
2023-09-27 08:19:37 +0530

    cp parachain

--- parachain/index.sh
+++ parachain/index.sh
@@ -34,8 +34,7 @@ build_parachain() {
 
 link_parachain() {
   echo "Linking parachain: sudo ln -s $HOME_DIR/parachan-bin /usr/local/bin..."
-  cd $repo_dir
-  cp ./target/release/$binary_name $HOME_DIR/parachan-bin
+  cp $HOME_DIR/$repo_dir/target/release/$binary_name $HOME_DIR/parachan-bin
   sudo chmod 777 $HOME_DIR/parachain-bin
   sudo ln -s $HOME_DIR/parachan-bin /usr/local/bin
   cd $HOME_DIR
@@ -55,7 +54,7 @@ install_and_build_zombienet() {
   then
     git clone https://github.com/paritytech/zombienet.git
   fi
-  
+
   cd zombienet/javascript
   yarn install
   yarn build
